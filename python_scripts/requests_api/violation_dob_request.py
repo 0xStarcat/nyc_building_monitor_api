6447f48e7060f6998f94a07441d312518b3c17f4
Jade <jeffahking@gmail.com>
2018-07-17 18:03:19 -0400

    fix violation request file

--- python_scripts/requests_api/violation_dob_request.py
+++ python_scripts/requests_api/violation_dob_request.py
@@ -3,21 +3,9 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from helpers import api_helpers
 from seeds import violations_seeds
-import sqlite3
-import config
 
-conn = sqlite3.connect(config.DATABASE_URL, timeout=10)
-
-# Boro = 3 (brooklyn)
-# Issued between dates
-# violation_type_code is not
-  # "c" (construction), 
-  # "LL5" or "LL5/83" (fire safety in office buildings), 
-  # "ES" (electric sign, probably business)
-
-table = violations_seeds.violations_table
-source = "DOB"
-dob_url = 'https://data.cityofnewyork.us/resource/dvnq-fhaa.json?$where=issue_date between "'+ api_helpers.get_next_day_to_request(conn, table, source) + '" and "' + api_helpers.get_today(table, source) + '" AND violation_type_code not in("LL5", "LL5/73", "ES")&'
-
-def make_request():
+def make_request(conn):
+  table = violations_seeds.violations_table
+  source = "DOB"
+  dob_url = 'https://data.cityofnewyork.us/resource/dvnq-fhaa.json?$where=issue_date between "'+ api_helpers.get_next_day_to_request(conn, table, source) + '" and "' + api_helpers.get_today(table, source) + '" AND violation_type_code not in("LL5", "LL5/73", "ES")&'
   return api_helpers.request_from_api(conn, dob_url, source, violations_seeds.seed_violations)
