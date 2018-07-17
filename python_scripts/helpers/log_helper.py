081ff901efc7e5f5c6dada053119490be6e5b99a
Jade <jeffahking@gmail.com>
2018-07-17 17:57:35 -0400

    oops forgot to append file flag

--- python_scripts/helpers/log_helper.py
+++ python_scripts/helpers/log_helper.py
@@ -6,5 +6,5 @@ def log_is_empty():
   return os.stat(config.LOG_URL).st_size == 0
 
 def write_to_log(string):
-  with open(config.LOG_URL) as log_file:
+  with open(config.LOG_URL, 'a') as log_file:
     log_file.write(string)
