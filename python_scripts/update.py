9940b8e56c5b1bb8dad4afdc9510599c2b651f95
Jade <jeffahking@gmail.com>
2018-08-06 19:51:06 -0400

    move email info out of try

--- python_scripts/update.py
+++ python_scripts/update.py
@@ -17,10 +17,10 @@ try:
 	status_update_counts = api_requests.check_call_statuses()
 	api_requests.update_data()
 
-	update_counts_data = { "date": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),**new_entry_counts, **status_update_counts}
-
-	conn = sqlite3.connect(config.DATABASE_BACKUP_URL, timeout=10)
-	c = conn.cursor()
+  conn = sqlite3.connect(config.DATABASE_BACKUP_URL, timeout=10)
+  c = conn.cursor()
+	
+  update_counts_data = { "date": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),**new_entry_counts, **status_update_counts}
 	context.updates_seeds.new_entry(c, update_counts_data)
 	conn.commit()
 
@@ -32,20 +32,17 @@ try:
 	context.log_helper.write_to_log("***\n")
 
 
-
-	username = os.environ["BUILDING_MONITOR_EMAIL"]
-	password = os.environ["BUILDING_MONITOR_EMAIL_PASSWORD"]
-	fromaddr = "NYC Building Monitor"
-	toaddrs  = "jadeahking@gmail.com"
-
 	log = open(config.LOG_URL, "r").read().split("***")
-	print(log)
 	msg = log[len(log) - 2]
 except Exception as error:
   msg = error
 
 print(msg)
 print("Message length is", len(msg))
+username = os.environ["BUILDING_MONITOR_EMAIL"]
+password = os.environ["BUILDING_MONITOR_EMAIL_PASSWORD"]
+fromaddr = "NYC Building Monitor"
+toaddrs  = "jadeahking@gmail.com"
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.set_debuglevel(1)
