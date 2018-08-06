0ccfd052da27f82afcc4678fdb095b16168ce773
Jade <jeffahking@gmail.com>
2018-08-06 09:34:35 -0400

    update

--- python_scripts/update.py
+++ python_scripts/update.py
@@ -9,41 +9,43 @@ import api_requests
 
 
 print("Starting data update...")
-shutil.copy(config.DATABASE_URL, config.DATABASE_BACKUP_URL)
+try:
+	shutil.copy(config.DATABASE_URL, config.DATABASE_BACKUP_URL)
 
-context.log_helper.write_to_log(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + '\n')
-new_entry_counts = api_requests.request(False)
-status_update_counts = api_requests.check_call_statuses()
-api_requests.update_data()
+	context.log_helper.write_to_log(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + '\n')
+	new_entry_counts = api_requests.request(False)
+	status_update_counts = api_requests.check_call_statuses()
+	api_requests.update_data()
 
-update_counts_data = { "date": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),**new_entry_counts, **status_update_counts}
+	update_counts_data = { "date": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),**new_entry_counts, **status_update_counts}
 
-conn = sqlite3.connect(config.DATABASE_BACKUP_URL, timeout=10)
-c = conn.cursor()
-context.updates_seeds.new_entry(c, update_counts_data)
-conn.commit()
+	conn = sqlite3.connect(config.DATABASE_BACKUP_URL, timeout=10)
+	c = conn.cursor()
+	context.updates_seeds.new_entry(c, update_counts_data)
+	conn.commit()
 
-shutil.move(config.DATABASE_URL, config.DATABASE_TEMP_URL)
-shutil.move(config.DATABASE_BACKUP_URL, config.DATABASE_URL)
-os.remove(config.DATABASE_TEMP_URL)
+	shutil.move(config.DATABASE_URL, config.DATABASE_TEMP_URL)
+	shutil.move(config.DATABASE_BACKUP_URL, config.DATABASE_URL)
+	os.remove(config.DATABASE_TEMP_URL)
 
-context.log_helper.write_to_log("Database replaced\n")
-context.log_helper.write_to_log("***\n")
+	context.log_helper.write_to_log("Database replaced\n")
+	context.log_helper.write_to_log("***\n")
 
 
 
-username = os.environ["BUILDING_MONITOR_EMAIL"]
-password = os.environ["BUILDING_MONITOR_EMAIL_PASSWORD"]
-fromaddr = "NYC Building Monitor"
-toaddrs  = "jadeahking@gmail.com"
+	username = os.environ["BUILDING_MONITOR_EMAIL"]
+	password = os.environ["BUILDING_MONITOR_EMAIL_PASSWORD"]
+	fromaddr = "NYC Building Monitor"
+	toaddrs  = "jadeahking@gmail.com"
 
-log = open(config.LOG_URL, "r").read().split("***")
-print(log)
-msg = log[len(log) - 2]
+	log = open(config.LOG_URL, "r").read().split("***")
+	print(log)
+	msg = log[len(log) - 2]
+except Exception as error:
+  msg = error
 
 print(msg)
 print("Message length is", len(msg))
-
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.set_debuglevel(1)
