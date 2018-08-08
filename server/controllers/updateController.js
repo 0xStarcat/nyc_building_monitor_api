b3196d32b5386ea4fcf9bb49b49639fc941226f6
Jade <jeffahking@gmail.com>
2018-08-08 14:58:59 -0400

    updates index endpoint

--- server/controllers/updateController.js
+++ server/controllers/updateController.js
@@ -2,6 +2,18 @@ const { db } = require(__dirname + '/../models/sequelize.js')
 const { constructUpdateJson } = require(__dirname + '/helpers/jsonHelpers.js')
 
 module.exports = {
+  index: async (req, res) => {
+    db.Update.findAll({
+      order: [['date', 'DESC']]
+    })
+      .then(data => {
+        res.json(constructUpdateJson(data))
+      })
+      .catch(data => {
+        console.log('ERROR', data)
+        res.json({ errors: data })
+      })
+  },
   last: async (req, res) => {
     db.Update.findAll({
       limit: 1,
--- server/routes/updatesRoutes.js
+++ server/routes/updatesRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const UpdateController = require(__dirname + '/../controllers/updateController')
 
+router.get('/', UpdateController.index)
 router.get('/last', UpdateController.last)
 
 module.exports = router
