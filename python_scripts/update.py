495f8499be08628f64d096ed6919f91d36301843
Jade <jeffahking@gmail.com>
2018-08-07 20:51:51 -0400

    indents

--- python_scripts/update.py
+++ python_scripts/update.py
@@ -10,30 +10,30 @@ import api_requests
 
 print("Starting data update...")
 try:
-	shutil.copy(config.DATABASE_URL, config.DATABASE_BACKUP_URL)
+  shutil.copy(config.DATABASE_URL, config.DATABASE_BACKUP_URL)
 
-	context.log_helper.write_to_log(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + '\n')
-	new_entry_counts = api_requests.request(False)
-	status_update_counts = api_requests.check_call_statuses()
-	api_requests.update_data()
+  context.log_helper.write_to_log(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + '\n')
+  new_entry_counts = api_requests.request(False)
+  status_update_counts = api_requests.check_call_statuses()
+  api_requests.update_data()
 
   conn = sqlite3.connect(config.DATABASE_BACKUP_URL, timeout=10)
   c = conn.cursor()
-	
+
   update_counts_data = { "date": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),**new_entry_counts, **status_update_counts}
-	context.updates_seeds.new_entry(c, update_counts_data)
-	conn.commit()
+  context.updates_seeds.new_entry(c, update_counts_data)
+  conn.commit()
 
-	shutil.move(config.DATABASE_URL, config.DATABASE_TEMP_URL)
-	shutil.move(config.DATABASE_BACKUP_URL, config.DATABASE_URL)
-	os.remove(config.DATABASE_TEMP_URL)
+  shutil.move(config.DATABASE_URL, config.DATABASE_TEMP_URL)
+  shutil.move(config.DATABASE_BACKUP_URL, config.DATABASE_URL)
+  os.remove(config.DATABASE_TEMP_URL)
 
-	context.log_helper.write_to_log("Database replaced\n")
-	context.log_helper.write_to_log("***\n")
+  context.log_helper.write_to_log("Database replaced\n")
+  context.log_helper.write_to_log("***\n")
 
 
-	log = open(config.LOG_URL, "r").read().split("***")
-	msg = log[len(log) - 2]
+  log = open(config.LOG_URL, "r").read().split("***")
+  msg = log[len(log) - 2]
 except Exception as error:
   msg = error
 
